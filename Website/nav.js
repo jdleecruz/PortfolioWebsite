6e659f5757e8a703fb9569c3e70a0ce9a183330d
John Dalton Lee-Cruz <jdleecruz@gmail.com>
2021-11-09 02:24:17 -0600

    Added a greyout effect in nav.js
    The hamburger button along with the nav will now follow throughout the webpage
    When clicking on the hamburger button the page will now 'grey out' until the hamburger button is clicked again or one of the links are clicked.

--- Website/nav.js
+++ Website/nav.js
@@ -1,11 +1,33 @@
 const navSlide = () => {
+    const grey = document.getElementById('greyout');
     const hamburger = document.querySelector('.hamburger');
+    const link = document.querySelectorAll('.nav-item');
     const nav = document.querySelector('.nav-list');
+    var isGrey = true;
 
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        if(isGrey === true) {
+            grey.style.display = "block";
+            isGrey = false;
+        } else {
+            grey.style.display = "none";
+            isGrey = true;
+        }
     });
 
+    link.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.toggle('nav-active');
+            if(isGrey === true) {
+                grey.style.display = "block";
+                isGrey = false;
+            } else {
+                grey.style.display = "none";
+                isGrey = true;
+            }
+        });
+    });
 }
 
 navSlide();
